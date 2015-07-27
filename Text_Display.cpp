d218bf6eb901f1a92d224ba9606ffb1d87b4c62f
JiaJun Zhou <jiajunzhou@JiaJundeMacBook-Pro.local>
2015-07-26 22:29:51 -0400

    updatee

--- Text_Display.cpp
+++ Text_Display.cpp
@@ -7,3 +7,21 @@
 //
 
 #include "Text_Display.h"
+#include <iostream>
+
+void Text_Display::Read_Command(){};
+
+void Text_Display::Print(std::string A_String){
+    for (int i=7; i>=0 ; i--) {
+        std::cout<<i+1<<" ";
+        for (int j=0; j<=7; j++) {
+            std::cout<<Borad[i][j]->Alias;
+        }
+        std::cout<<std::endl;
+        std::cout<<"  abcdefgh"<<std::endl;
+    }
+}
+
+Text_Display::Text_Display(){};
+
+Text_Display::~Text_Display(){};
